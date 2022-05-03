41b26edfe22e1746b83534a922db93aca92fd0e6
vishal <vishalpalla27@gmail.com>
2022-05-04 00:46:56 +0530

    Navigation section

---
+++ react-native-navigation/screens/MealsOverviewScreen.js
@@ -0,0 +1,34 @@
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
+
+export default MealsOverviewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        item={itemData.item}
+      />
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
